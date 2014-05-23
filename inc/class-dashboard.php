833b56716ca2d4b96cb625fec5e10e154c1a0fef
Fikri Rasyid <fikrirasyid@gmail.com>
2014-05-23 07:58:18 +0700

    Adding authorization and deauthorization mechnanism

--- inc/class-dashboard.php
+++ inc/class-dashboard.php
@@ -7,12 +7,17 @@ class WP_IG_Dashboard{
 	var $prefix;
 	var $client_id;
 	var $client_secret;
+	var $authorization_url;
+	var $exchange_code;
 
 	function __construct(){
-		$this->prefix = 'wp_ig_';
-		$this->redirect_uri = admin_url() . 'admin-ajax.php?action=wp_ig_redirect_uri';
-		$this->client_id = get_option( "{$this->prefix}client_id" );
-		$this->client_secret = get_option( "{$this->prefix}client_secret" );
+		$this->prefix 				= 'wp_ig_';
+		$this->client_id 			= get_option( "{$this->prefix}client_id" );
+		$this->client_secret 		= get_option( "{$this->prefix}client_secret" );
+		$this->redirect_uri 		= admin_url() . 'admin-ajax.php?action=wp_ig_redirect_uri';
+		$this->redirect_uri_encoded = urlencode( $this->redirect_uri );
+		$this->authorization_url 	= "https://api.instagram.com/oauth/authorize/?client_id={$this->client_id}&redirect_uri={$this->redirect_uri_encoded}&response_type=code";		
+		$this->exchange_code 		= get_option( "{$this->prefix}exchange_code" );
 
 		// If user is currently on wp_ig pages
 		if( isset( $_GET['page'] ) && substr( $_GET['page'], 0, 5 ) == "wp_ig" ){
@@ -21,7 +26,12 @@ class WP_IG_Dashboard{
 		}
 
 		// Register dashboard menu
+		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_scripts_styles' ) );
 		add_action( 'admin_menu', array( $this, 'register_pages' ) );
+
+		// Processing auth-redirect data
+		add_action( 'wp_ajax_wp_ig_redirect_uri', array( $this, 'page_redirect_callback' ) );
+		add_action( 'wp_ajax_wp_ig_deauth_account', array( $this, 'page_deauth' ) );
 	}
 	
 	/**
@@ -46,8 +56,8 @@ class WP_IG_Dashboard{
 	function saving(){
 		// Updating value..
 		if( isset( $_POST['client_id'] ) && isset( $_POST['client_secret'] ) && isset( $_POST['_wpnonce'] ) && wp_verify_nonce( $_POST['_wpnonce'], 'wp_ig_setup' ) ){
-			$saving_client_id = update_option( "wp_ig_client_id", sanitize_text_field( $_POST['client_id'] ) );
-			$saving_client_secret = update_option( "wp_ig_client_secret", sanitize_text_field( $_POST['client_secret'] ) );
+			$saving_client_id = update_option( "{$this->prefix}client_id", sanitize_text_field( $_POST['client_id'] ) );
+			$saving_client_secret = update_option( "{$this->prefix}client_secret", sanitize_text_field( $_POST['client_secret'] ) );
 
 			if( $saving_client_id ){
 				$this->client_id = sanitize_text_field( $_POST['client_id'] );
@@ -66,6 +76,17 @@ class WP_IG_Dashboard{
 	}
 
 	/**
+	 * Enqueue scripts & stylesheets
+	 * 
+	 * @return void
+	 */
+	function enqueue_scripts_styles( $hook ){
+		if( $hook == 'toplevel_page_wp_ig' ){
+			wp_enqueue_style( 'wp_ig_dashboard', WP_IG_URL . 'css/dashboard.css', false, '1.0.0' );
+		}
+	}
+
+	/**
 	 * Print main / setup page of WP-IG
 	 * 
 	 * @return void
@@ -77,7 +98,8 @@ class WP_IG_Dashboard{
 					// First time user: create client instruction + client ID + client secret
 					include_once( WP_IG_DIR . '/pages/setup.php' );					
 				} else {
-					// Client ID & secret found: connect your account
+					// Display profile
+					include_once( WP_IG_DIR . '/pages/profile.php' );					
 
 					// Connected: preview your account					
 				}
@@ -92,6 +114,92 @@ class WP_IG_Dashboard{
 	 */
 	function page_setup(){
 		include_once( WP_IG_DIR . '/pages/setup.php' );
+	}
+
+	/**
+	 * Handling redirection of Instagram authentication process
+	 * 
+	 * @return void
+	 */
+	function page_redirect_callback(){
+		if( isset( $_GET['code'] ) ){
+			// Save the exchange code
+			$save_exchange_code = update_option("{$this->prefix}exchange_code", esc_html( $_GET['code'] ) );
+
+			// Request access token
+			$args = array(
+				'timeout' => 60,
+				'body' => array(
+					'client_id' => $this->client_id,
+					'client_secret' => $this->client_secret,
+					'grant_type' => 'authorization_code',
+					'redirect_uri' => $this->redirect_uri,
+					'code' => $this->exchange_code
+				)
+			);
+
+			// Request access token to Instagram
+			$request_access_token = wp_remote_post( "https://api.instagram.com/oauth/access_token", $args );
+
+			if( isset( $request_access_token['body'] ) && $request_access_token['response']['code'] == '200' ){
+				// Parse the body response
+				$response = json_decode( $request_access_token['body'] );
+
+				if( isset( $response->access_token ) ){
+					// Save the access token
+					$save_access_token = update_option( "{$this->prefix}access_token", esc_html( $response->access_token ) );
+
+					// Save the user info
+					$save_user = update_option( "{$this->prefix}account", $response->user );
+
+					// Close the popup, then reload the parent page
+					?>
+						<script type="text/javascript">
+						   opener.location.reload(true);
+						   self.close();
+						</script>
+					<?php
+				} else {
+					_e( "Fail to connect to your Instagram Account. Please try again", "wp_ig" );
+				}
+			} else {
+				_e( "We cannot connect to Instagram. Please close this popup window then try again.", "wp_ig" );
+			}
+
+			die();			
+		} elseif( isset( $_GET['error'] ) && isset( $_GET['error_description'] ) ){
+			// If user denied to give an access...
+			echo $_GET['error_description'];
+
+		} else {
+			_e( "An error occurred. We cannot connect this site to your Instagram account. Please try again", "wp_ig" );
+		}
+
+		die();		
+	}
+
+	function page_deauth(){
+
+		// Disconnecting...
+		if( isset( $_GET['_wpnonce'] ) && wp_verify_nonce( $_GET['_wpnonce'], 'deauth_instagram_account' ) ){
+			_e( "Disconnecting your instagram account...", "wp_ig" );
+
+			// remove the data
+			delete_option( "{$this->prefix}exchange_code" );
+			delete_option( "{$this->prefix}access_token" );
+			delete_option( "{$this->prefix}account" );
+		} else {			
+			_e( "You are not authorized to disconnect the instagram account from this site...", "wp_ig" );
+		}
+
+		// Close the popup, then reload the parent page
+		?>
+			<script type="text/javascript">
+			   opener.location.reload(true);
+			   self.close();
+			</script>
+		<?php		
+		die();
 	}
 }
 new WP_IG_Dashboard;
---
+++ pages/profile.php
@@ -0,0 +1,51 @@
+<div class="wrap">
+	<?php
+		$account = get_option( "{$this->prefix}account" );
+		$access_token = get_option( "{$this->prefix}access_token" );
+
+		// Display connect or profile page based on current user log in state
+		if( $account ) :
+	?>
+	
+	<h2>Your Account</h2>
+
+	<div id="current-instagram-profile">
+		<div class="avatar">
+			<img src="<?php echo $account->profile_picture; ?>" alt="">
+		</div>
+		<div class="data">
+			<h3 class="full-name"><?php echo $account->full_name; ?></h3>
+			<h4 class="username"><a href="http://instagram.com/<?php echo $account->username; ?>" title="<?php echo $account->username; _e( " on Instagram", "wp_ig" ); ?>"><?php echo $account->username; ?></a> - <a href="<?php echo $account->website; ?>"><?php echo $account->website; ?></a></h4>
+			<?php if( !empty( $account->website ) ) : ?>
+			<?php endif; ?>
+			<div class="bio">
+				<?php echo wpautop( $account->bio ); ?>
+			</div>			
+
+			<a href="#" id="deauth-instagram">Disconnect this account</a>
+		</div>
+	</div>
+
+	<script type="text/javascript">
+		jQuery(document).ready(function($){
+			$('#deauth-instagram').click(function(e){
+				InstagramAuthWindow = window.open('<?php echo admin_url(); ?>admin-ajax.php?action=wp_ig_deauth_account&_wpnonce=<?php echo wp_create_nonce( "deauth_instagram_account" ); ?>', 'Instagram Authorization', 'width=800,height=400');	
+			});
+		});
+	</script>	
+
+	<?php else : ?>
+
+	<h2><?php _e( "Connect Your Account", "wp_ig" ); ?></h2>
+
+	<p><a href="#" id="auth-instagram" title="<?php _e( "Connect To Instagram", "wp_ig" ); ?>"><?php _e( "Connect To Instagram", "wp_ig" ); ?></a></p>
+	<script type="text/javascript">
+		jQuery(document).ready(function($){
+			$('#auth-instagram').click(function(e){
+				InstagramAuthWindow = window.open('<?php echo $this->authorization_url; ?>', 'Instagram Authorization', 'width=800,height=400');	
+			});
+		});
+	</script>	
+
+	<?php endif; ?>
+</div>
