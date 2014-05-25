7d61884105b67b28b6c2050e31b9d87eabb1336b
Fikri Rasyid <fikrirasyid@gmail.com>
2014-05-25 23:21:11 +0700

    Deleting max_id method
    Deleting max_id method on WP_IG_QueryStrings since get() can do its job
    easily

--- inc/class-querystrings.php
+++ inc/class-querystrings.php
@@ -1,21 +1,6 @@
 <?php
 class WP_IG_QueryStrings{
 	/**
-	 * Get max id
-	 * 
-	 * @author Fikri Rasyid
-	 * 
-	 * return bool|string
-	 */
-	function max_id(){
-		if( isset( $_GET['max_id'] ) ){
-			return $_GET['max_id'];
-		} else {
-			return false;
-		}
-	}
-
-	/**
 	 * Get query string value. Return as false if no query string is stated
 	 * 
 	 * @author Fikri Rasyid
