d656702bff6b9e81b79d9e397f2a1376dee1c173
Fikri Rasyid <fikrirasyid@gmail.com>
2014-05-29 08:36:38 +0700

    Adding public page for instagram content
    - Adding public page for instagram content
    - generating more link based on response->pagination->next_url instead
    of manually crafting it

--- inc/class-public.php
+++ inc/class-public.php
@@ -3,6 +3,10 @@ class WP_IG_Public{
 
 	function __construct(){
 		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_script_styles' ) );		
+
+		// Adding public page for instagram content
+		add_action( 'wp_ajax_instagram', array( $this, 'public_page' ) );
+		add_action( 'wp_ajax_nopriv_instagram', array( $this, 'public_page' ) );
 	}
 
 	/**
@@ -12,6 +16,23 @@ class WP_IG_Public{
 	 */	
 	function enqueue_script_styles(){
 		wp_enqueue_style( 'wp_ig', WP_IG_URL . 'css/wp-ig.css' );
+	}
+
+	/**
+	 * Adding public page for instagram content
+	 * 
+	 * @return void
+	 */
+	function public_page(){
+		get_header();
+
+		$shortcode = new WP_IG_Shortcodes;
+
+		echo $shortcode->user_media( $_REQUEST );
+
+		get_footer();
+
+		die();
 	}
 }
 new WP_IG_Public;
