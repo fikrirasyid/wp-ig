ec3c65310749811a709190b7744129c206f2f395
Fikri Rasyid <fikrirasyid@gmail.com>
2014-07-02 19:46:33 +0700

    Registering "mention" taxonomy

--- inc/class-dashboard.php
+++ inc/class-dashboard.php
@@ -36,6 +36,9 @@ class WP_IG_Dashboard{
 		// Processing auth-redirect data
 		add_action( 'wp_ajax_wp_ig_redirect_uri', array( $this, 'page_redirect_callback' ) );
 		add_action( 'wp_ajax_wp_ig_deauth_account', array( $this, 'page_deauth' ) );
+
+		// Register custom taxonomy
+		add_action( 'init', array( $this, 'register_taxonomy' ) );
 	}
 	
 	/**
@@ -52,6 +55,42 @@ class WP_IG_Dashboard{
 			add_submenu_page( 'wp_ig', __( 'Delete', 'wp-ig' ), __( 'Delete', 'wp-ig' ), 'edit_others_posts', 'wp_ig_delete', array( $this, 'page_delete') );
 			add_submenu_page( 'wp_ig', __( 'Setup', 'wp-ig' ), __( 'Setup', 'wp-ig' ), 'edit_others_posts', 'wp_ig_setup', array( $this, 'page_setup') );
 		}
+	}
+
+	/**
+	 * Register taxonomy
+	 * 
+	 * @return void
+	 */
+	function register_taxonomy(){
+
+		// Define post type
+		$post_types = array( get_option( 'wp_ig_post_type', 'post' ) );
+
+		// Register taxonomy
+		$labels = array(
+			'name'                       => _x( 'Mentions', 'taxonomy general name', 'wp-ig' ),
+			'singular_name'              => _x( 'Mention', 'taxonomy singular name', 'wp-ig' ),
+			'search_items'               => __( 'Search Mentions', 'wp-ig' ),
+			'popular_items'              => __( 'Popular Mentions', 'wp-ig' ),
+			'all_items'                  => __( 'All Mentions', 'wp-ig' ),
+			'parent_item'                => null,
+			'parent_item_colon'          => null,
+			'edit_item'                  => __( 'Edit Mention', 'wp-ig' ),
+			'update_item'                => __( 'Update Mention', 'wp-ig' ),
+			'add_new_item'               => __( 'Add New Mention', 'wp-ig' ),
+			'new_item_name'              => __( 'New Mention Name', 'wp-ig' ),
+			'separate_items_with_commas' => __( 'Separate mentions with commas', 'wp-ig' ),
+			'add_or_remove_items'        => __( 'Add or remove mentions', 'wp-ig' ),
+			'choose_from_most_used'      => __( 'Choose from the most used mentions', 'wp-ig' ),
+			'not_found'                  => __( 'No mentions found.', 'wp-ig' ),
+			'menu_name'                  => __( 'Mentions', 'wp-ig' ),
+		);
+
+		register_taxonomy( 'mention', apply_filters( 'wp_ig_post_type_taxonomy', $post_types ), array(
+			'labels' 		=> $labels,
+			'hierarchical' 	=> false,
+		) );
 	}
 
 	/**
