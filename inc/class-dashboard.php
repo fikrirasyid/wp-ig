6f8b8b580c271be6a3a65c03e8dea42e1012b486
Fikri Rasyid <fikrirasyid@gmail.com>
2014-07-10 04:46:11 +0700

    Flushing rewrite URL upon installation
    Making sure that mention taxonomy is properly flushed upon plugin
    activation

--- inc/class-dashboard.php
+++ inc/class-dashboard.php
@@ -74,34 +74,8 @@ class WP_IG_Dashboard{
 	 * @return void
 	 */
 	function register_taxonomy(){
-
-		// Define post type
-		$post_types = array( get_option( 'wp_ig_post_type', 'post' ) );
-
-		// Register taxonomy
-		$labels = array(
-			'name'                       => _x( 'Mentions', 'taxonomy general name', 'wp-ig' ),
-			'singular_name'              => _x( 'Mention', 'taxonomy singular name', 'wp-ig' ),
-			'search_items'               => __( 'Search Mentions', 'wp-ig' ),
-			'popular_items'              => __( 'Popular Mentions', 'wp-ig' ),
-			'all_items'                  => __( 'All Mentions', 'wp-ig' ),
-			'parent_item'                => null,
-			'parent_item_colon'          => null,
-			'edit_item'                  => __( 'Edit Mention', 'wp-ig' ),
-			'update_item'                => __( 'Update Mention', 'wp-ig' ),
-			'add_new_item'               => __( 'Add New Mention', 'wp-ig' ),
-			'new_item_name'              => __( 'New Mention Name', 'wp-ig' ),
-			'separate_items_with_commas' => __( 'Separate mentions with commas', 'wp-ig' ),
-			'add_or_remove_items'        => __( 'Add or remove mentions', 'wp-ig' ),
-			'choose_from_most_used'      => __( 'Choose from the most used mentions', 'wp-ig' ),
-			'not_found'                  => __( 'No mentions found.', 'wp-ig' ),
-			'menu_name'                  => __( 'Mentions', 'wp-ig' ),
-		);
-
-		register_taxonomy( 'mention', apply_filters( 'wp_ig_post_type_taxonomy', $post_types ), array(
-			'labels' 		=> $labels,
-			'hierarchical' 	=> false,
-		) );
+		$taxonomy = new WP_IG_Taxonomy;
+		$taxonomy->register();
 	}
 
 	/**
